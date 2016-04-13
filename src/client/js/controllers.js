aab9704b4ad5ed4e81bf25dab454a13918eecfcb
David Sudia <dsudia@gmail.com>
2016-04-13 15:59:07 -0600

    site displays students and can add a student

--- src/client/js/controllers.js
+++ src/client/js/controllers.js
@@ -1,6 +1,25 @@
 /**
  * Created by davidsudia on 4/13/16.
  */
-app.controller('myController', ['$scope', ($scope) => {
-  $scope.greeting = 'Angulaaaaaaaaar!';
+app.controller('addStudentController', ['$scope', 'studentDataService', ($scope, studentDataService) => {
+
+  $scope.student= {};
+
+  studentDataService.getAllStudents()
+    .then((students) => {
+      $scope.allStudents = students.data.data;
+    });
+
+  $scope.addStudent = () => {
+    studentDataService.addStudent($scope.student)
+      .then((data) => {
+        studentDataService.getAllStudents()
+          .then((students) => {
+            $scope.allStudents = students.data.data;
+            $scope.student = {};
+          });
+      });
+
+  }
+
 }]);
--- src/client/js/services.js
+++ src/client/js/services.js
@@ -1,13 +1,52 @@
 /**
  * Created by davidsudia on 4/13/16.
  */
-app.service('dataService', ['$http', ($http) => {
+app.service('studentDataService', ['owleryService', (owleryService) => {
 
-  var data = [];
-  
   return {
-    getAllStudents: function() {
-      return data;
+
+    getAllStudents: () => {
+      return owleryService.getAll('students')
+        .then((students) => {
+          return students;
+        });
+    },
+
+    addStudent: (payload) => {
+      return owleryService.addOne('students', payload)
+        .then((student) => {
+          return student;
+        })
+    }
+
+  }
+}]);
+
+
+app.service('owleryService', ['$http', ($http) => {
+
+  return {
+
+    getAll: (resource) => {
+      return $http.get('/' + resource)
+        .catch((err) => {
+          return err;
+        })
+        .then((res) => {
+          return res;
+        });
+    },
+
+    addOne: (resource, payload) => {
+      var arr = []
+      arr.push(payload);
+      return $http.post('/' + resource, arr)
+        .catch((err) => {
+          return err;
+        })
+        .then((res) => {
+          return res;
+        });
     }
   }
 
