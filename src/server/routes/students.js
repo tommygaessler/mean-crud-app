ca52ffb6ec3a157d81600478ebc8937b82aff0ca
David Sudia <dsudia@gmail.com>
2016-04-13 13:37:47 -0600

    all crud tests written and passed

--- src/server/routes/students.js
+++ src/server/routes/students.js
@@ -20,15 +20,41 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  console.log(req.body);
   Students.insertMany(req.body, (err, newStudents) => {
     if (err) {
       return next(err);
     }
     res.status(200).json({
       status: 'success'
-    })
+    });
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  var id = req.params.id;
+  option = req.body;
+  console.log('option', option);
+  Students.findByIdAndUpdate(id, option, (err, update) => {
+    if(err) {
+      return next(err);
+    }
+    res.status(200).json({
+      status: 'success'
+    });
   })
+});
+
+router.delete('/remove/:id', (req, res, next) => {
+  console.log('got request');
+  var id = req.params.id;
+  Students.findOneAndRemove(id, function(err, student) {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({
+      status: 'success'
+    });
+  });
 });
 
 module.exports = router;
--- test/integration/students-routes.js
+++ test/integration/students-routes.js
@@ -23,12 +23,12 @@ describe('student routes', function() {
   });
 
 
-  describe('/GET students', function() {
+  describe('GET /students', function() {
 
-    it('it should return all students', (done) => {
+    it('should return all students on a GET request', (done) => {
       chai.request(server)
         .get('/students')
-        .then(function(res) {
+        .then(function (res) {
           res.status.should.equal(200);
           res.type.should.equal('application/json');
           res.body.should.be.a('object');
@@ -42,42 +42,122 @@ describe('student routes', function() {
           done();
         });
     });
+  });
 
-    it('it should put a new student into the db', (done) => {
+  describe('POST /students', function() {
+
+    it('should put a new student into the db on a POST request', (done) => {
       chai.request(server)
         .post('/students')
         .send([{firstName: 'Draco', lastName: 'Malfoy', year: 1}])
-        .then(function(res) {
+        .then(function (res) {
           res.status.should.equal(200);
           res.type.should.equal('application/json');
           res.body.should.be.a('object');
           res.body.should.have.property('status');
           res.body.status.should.equal('success');
-          done();
-      })
-        .catch(function(err) {
-          console.log(err);
         })
-        .then(function(data) {
-        chai.request(server)
-          .get('/students')
-          .end(function(err, res) {
-            res.status.should.equal(200);
-            res.type.should.equal('application/json');
-            res.body.should.be.a('object');
-            res.body.should.have.property('status');
-            res.body.status.should.equal('success');
-            res.body.should.have.property('data');
-            res.body.data.length.should.equal(4);
-            res.body.data[4].firstName.should.equal('Harry');
-            res.body.data[4].lastName.should.equal('Potter');
-            res.body.data[4].year.should.equal(1);
-            done();
-          });
-      })
-    })
+        .catch(function (err) {
+          console.log(err.original);
+        })
+        .then(function (data) {
+          chai.request(server)
+            .get('/students')
+            .end(function (err, res) {
+              res.status.should.equal(200);
+              res.type.should.equal('application/json');
+              res.body.should.be.a('object');
+              res.body.should.have.property('status');
+              res.body.status.should.equal('success');
+              res.body.should.have.property('data');
+              res.body.data.length.should.equal(4);
+              res.body.data[3].firstName.should.equal('Draco');
+              res.body.data[3].lastName.should.equal('Malfoy');
+              res.body.data[3].year.should.equal(1);
+              done();
+            });
+        });
+    });
+  });
 
-    
+  describe('PUT /students', function() {
 
+    it('should update a student on a PUT request', (done) => {
+      chai.request(server)
+        .get('/students')
+        .then(function(students) {
+          var studentID = students.body.data[0]._id;
+          chai.request(server)
+            .put('/students/' + studentID)
+            .send({year: 2})
+            .then(function(res) {
+              res.status.should.equal(200);
+              res.type.should.equal('application/json');
+              res.body.should.be.a('object');
+              res.body.should.have.property('status');
+              res.body.status.should.equal('success');
+            })
+            .catch(function(err) {
+              console.log(err.original);
+            })
+            .then(function(data) {
+              chai.request(server)
+                .get('/students')
+                .end(function (err, res) {
+                  res.status.should.equal(200);
+                  res.type.should.equal('application/json');
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('status');
+                  res.body.status.should.equal('success');
+                  res.body.should.have.property('data');
+                  res.body.data.length.should.equal(3);
+                  res.body.data[0].firstName.should.equal('Harry');
+                  res.body.data[0].lastName.should.equal('Potter');
+                  res.body.data[0].year.should.equal(2);
+                  done();
+                });
+            });
+        });
+    });
+  });
+
+  describe ('DELETE /students', function() {
+
+    it('should delete a student on a DELETE request', (done) => {
+      chai.request(server)
+        .get('/students')
+        .then(function(students) {
+          var studentID = students.body.data[0]._id;
+          chai.request(server)
+            .delete('/students/remove/' + studentID)
+            .then(function(res) {
+              res.status.should.equal(200);
+              res.type.should.equal('application/json');
+              res.body.should.be.a('object');
+              res.body.should.have.property('status');
+              res.body.status.should.equal('success');
+            })
+            .catch(function(err) {
+              console.log(err.original);
+            })
+            .then(function(data) {
+              chai.request(server)
+                .get('/students')
+                .end(function (err, res) {
+                  res.status.should.equal(200);
+                  res.type.should.equal('application/json');
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('status');
+                  res.body.status.should.equal('success');
+                  res.body.should.have.property('data');
+                  res.body.data.length.should.equal(2);
+                  res.body.data[0].firstName.should.equal('Ron');
+                  res.body.data[0].lastName.should.equal('Weasley');
+                  res.body.data[0].year.should.equal(1);
+                  done();
+                });
+            });
+        });
+    });
   });
 });
