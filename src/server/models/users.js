8aeb29f32e6bfc0b32624cac06f484868ab5fea7
David Sudia <dsudia@gmail.com>
2016-04-14 13:33:03 -0600

    created user schema for mongoose

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -1,23 +1,28 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+var bcrypt = require('bcrypt');
 
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   admin: {
-    type: bool,
+    type: Boolean,
     default: false,
     required: true
   }
 });
+// hash the password before saving it to the db
+
+// compare password to verify plain text against the hashed password
 
 var User = mongoose.model('students', UserSchema);
 
